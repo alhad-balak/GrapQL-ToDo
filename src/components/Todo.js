dcad0d3baa787b57fc22ac0f1ceb9df6dfff751b
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-05-05 23:37:57 +0530

    Function task completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,8 +2,54 @@ import React from 'react'
 import AddTodos from './AddTodos'
 
 const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const LOCAL_STORAGE_KEY = "react-do-list-todos";
+
+    useEffect(() => {
+        const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if (storageTodos) {
+            setTodos(storageTodos)
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+    }, [todos]);
+
+    const addTodo = todo => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+        const newTodos = [todo, ...todos];
+        setTodos(newTodos);
+        console.log(...todos);
+    };
+
+    const updateTodo = (todoId, newValue) => {
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+            return
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id);
+        setTodos(removeArr);
+    };
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo;
+        })
+        setTodos(updatedTodos);
+    };
     return (
-        <div>
+        <div className="todo-context">
+            <h1>Things to do Today.</h1>
             <AddTodos onSubmit={addTodo} />
         </div>
     )
