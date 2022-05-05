1477336986eb15033af8fb4b2f856e29c6d1147b
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-05-06 00:07:25 +0530

    Completed!

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -24,7 +24,7 @@ const Todo = () => {
         }
         const newTodos = [todo, ...todos];
         setTodos(newTodos);
-        console.log(...todos);
+        // console.log(...todos);
     };
 
     const updateTodo = (todoId, newValue) => {
